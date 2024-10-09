34ca9094933ea357db6235b1785438de1493c89b
TheSaviourEking <saviour.eking@outlook.com>
2024-10-09 15:45:57 +0100

    chore: ready for deploy

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,3 @@
-# app/__init__.py
-
 import os
 from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
@@ -14,7 +12,7 @@ def create_app():
 
     # Comment out cors for production we serving from same server
     # CORS(app)
-    
+
     app.config.from_object(Config)
 
     db.init_app(app)
@@ -25,6 +23,14 @@ def create_app():
 
     frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
     dist_folder = os.path.join(frontend_folder, "dist")
+
+    # Serve Static files from dist folder
+    # @app.route("/", defaults={"filename": ""})
+    # @app.route("/<path:filename>")
+    # def index(filename):
+    #     if not filename:
+    #         filename = "index.html"
+    #
 
     @app.route("/", defaults={"path": ""})
     @app.route("/<path:path>")
@@ -41,13 +47,3 @@ def create_app():
         db.create_all()
 
     return app
-
-
-# run.py
-
-# from app import create_app
-
-# app = create_app()
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,9 +1,3 @@
-# from app.__init__ import app
-
-# if __name__ == "__main__":
-#     app.run
-
-
 from app import create_app
 
 app = create_app()
