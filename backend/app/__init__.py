f336b282d1fd22e9b12b5a6314eb227d1fd4c52b
TheSaviourEking <saviour.eking@outlook.com>
2024-10-09 15:12:38 +0100

    chore: ready for deployment

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -44,8 +44,10 @@ def serve(path):
 # api routes
 
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
+
+db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
